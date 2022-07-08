e6895f91a6ed6d48940618ed3d701604fe029fc5
Marcos Diaz <marcos.dure.diaz@gmail.com>
2022-07-08 14:41:35 -0300

    changed jax_experiments to ensure JAX has finished

--- jax_experiments/jax_sum_performance_test.py
+++ jax_experiments/jax_sum_performance_test.py
@@ -13,6 +13,7 @@ def perf_timer(func):
     def wrapper_timer(*args, **kwargs):
         tic = time.perf_counter()
         value = func(*args, **kwargs)
+        print(f"({func.__name__}) value: {value}")
         toc = time.perf_counter()
         elapsed_time = toc - tic
         print(f"({func.__name__}) Elapsed time: {elapsed_time:2.10f} seconds")
@@ -25,6 +26,7 @@ def perf_timer_jax(func):
     def wrapper_timer(*args, **kwargs):
         tic = time.perf_counter()
         value = func(*args, **kwargs).block_until_ready()
+        print(f"({func.__name__}) value: {value}")
         toc = time.perf_counter()
         elapsed_time = toc - tic
         print(f"({func.__name__}) Elapsed time: {elapsed_time:2.10f} seconds")
@@ -61,6 +63,11 @@ def jax_random_sum_jit_device_put(array):
 def jax_random_sum_batched(array):
     rand_sum = jnp.sum(array, axis=0)
     rand_sum = jnp.sum(rand_sum)
+    return rand_sum
+
+def numpy_random_sum_batched(array):
+    rand_sum = np.sum(array, axis=0)
+    rand_sum = np.sum(rand_sum)
     return rand_sum
 
 
@@ -105,28 +112,30 @@ if __name__ == "__main__":
     batched_arr = jnp.reshape(rand_vec_jax, (4, -1))
     dummy = random.normal(dummy_key, (1, int(arrary_size)))
 
-    print(f'sum: {perf_timer(numpy_random_sum)(rand_vec_numpy)}')
+    (f'sum: {perf_timer(numpy_random_sum)(rand_vec_numpy)}')
 
-    print(f'sum: {perf_timer_jax(jax_random_sum)(rand_vec_jax)}')
+    (f'sum: {perf_timer_jax(jax_random_sum)(rand_vec_jax)}')
 
-    print(f'sum: {perf_timer_jax(jax_random_sum_device_put)(rand_vec_jax)}')
+    (f'sum: {perf_timer_jax(jax_random_sum_device_put)(rand_vec_jax)}')
 
     jax_random_sum_jit(dummy)  # discard first run
-    print(f'sum: {perf_timer_jax(jax_random_sum_jit)(rand_vec_jax)}')
+    (f'sum: {perf_timer_jax(jax_random_sum_jit)(rand_vec_jax)}')
 
     jax_random_sum_jit_device_put(dummy)  # discard first run
-    print(f'sum: {perf_timer_jax(jax_random_sum_jit_device_put)(rand_vec_jax)}')
+    (f'sum: {perf_timer_jax(jax_random_sum_jit_device_put)(rand_vec_jax)}')
 
-    print(f'sum: {perf_timer_jax(jax_random_sum_batched)(batched_arr)}')
+    (f'sum: {perf_timer(numpy_random_sum_batched)(rand_vec_numpy)}')
+
+    (f'sum: {perf_timer_jax(jax_random_sum_batched)(batched_arr)}')
 
     jax_random_sum_vmap(dummy)  # discard first run
-    print(f'sum: {perf_timer_jax(jax_random_sum_vmap)(batched_arr)}')
+    (f'sum: {perf_timer_jax(jax_random_sum_vmap)(batched_arr)}')
 
     jax_random_sum_vmap_jit(dummy)  # discard first run
-    print(f'sum: {perf_timer_jax(jax_random_sum_vmap_jit)(batched_arr)}')
+    (f'sum: {perf_timer_jax(jax_random_sum_vmap_jit)(batched_arr)}')
 
     jax_random_sum_pmap(dummy)  # discard first run
-    print(f'sum: {perf_timer_jax(jax_random_sum_pmap)(batched_arr)}')
+    (f'sum: {perf_timer_jax(jax_random_sum_pmap)(batched_arr)}')
 
     jax_random_sum_pmap_jit(dummy)  # discard first run
     print(f'sum: {perf_timer_jax(jax_random_sum_pmap_jit)(batched_arr)}')
